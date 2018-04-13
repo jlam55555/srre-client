e28dab8be59c3971c5d76118bf2d811b564ee75a
Jonathan Lam <jlam55555@gmail.com>
2018-04-13 10:15:51 -0400

    added basic form for volunteer page

--- src/app/volunteer/volunteer.component.ts
+++ src/app/volunteer/volunteer.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PageService } from '../page.service';
+import { ServerService } from '../server.service';
 
 @Component({
   selector: 'app-volunteer',
@@ -8,13 +9,21 @@ import { PageService } from '../page.service';
 })
 export class VolunteerComponent implements OnInit {
 
-  constructor(private pageService: PageService) { }
+  constructor(private pageService: PageService, private serverService: ServerService) { }
 
+  // to determine whether or not you can see this content
+  // both need to be true
   public isSignedIn: boolean = false;
+  public isVolunteer: boolean = false;
+  public volunteerTeam: string = '';
 
   ngOnInit() {
     this.pageService.setPageTitle('Volunteer');
     this.pageService.getIsSignedIn().subscribe(newIsSignedIn => this.isSignedIn = newIsSignedIn);
+    this.serverService.getUserDetails().subscribe(newUserDetails => {
+      this.volunteerTeam = newUserDetails.team;
+      this.isVolunteer = newUserDetails.team !== null;
+    });
   }
 
 }
