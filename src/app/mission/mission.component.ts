188e0715dd2fa20e3af6031d42a71462c5b8b4c4
Jonathan Lam <jlam55555@gmail.com>
2018-04-14 00:57:22 -0400

    updated mission with times from db

--- src/app/mission/mission.component.ts
+++ src/app/mission/mission.component.ts
@@ -15,7 +15,28 @@ export class MissionComponent implements OnInit {
   public mission: any = {};
 
   ngOnInit() {
-    this.serverService.getMissionData().subscribe(missionData => this.mission = missionData);
+    this.serverService.getMissionData().subscribe(missionData => {
+
+      // make waypoints into array
+      missionData.w = [
+        { time: missionData.w0, next: false, isConfirmRole: true, place: missionData.meetingplace, text: `${missionData.driver1} and ${missionData.driver2} have left the starting place.` },
+        { time: missionData.w1, next: false, isConfirmRole: true, place: missionData.startplace, text: `${missionData.drivee} has been picked up.` },
+        { time: missionData.w2, next: false, isConfirmRole: false, place: missionData.endplace, text: `${missionData.drivee} has been dropped off at his/her home.` },
+        { time: missionData.w3, next: false, isConfirmRole: false, place: missionData.meetingplace, text: `${missionData.driver1} and ${missionData.driver2} have returned to the starting place.` }
+      ];
+
+      // set next element
+      let nextId;
+      if(missionData.w0 === null) nextId = 0;
+      else if(missionData.w1 === null) nextId = 1;
+      else if(missionData.w2 === null) nextId = 2;
+      else nextId = 3;
+      missionData.nextId = nextId;
+      missionData.w[nextId].next = true;
+
+      // set mission to mission data
+      this.mission = missionData;
+    });
 
     // configure card  collapse indicators
     this.pageService.collapseIndicators();
